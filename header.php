de4c3c9ab5f5c0a168c038846e9ab717ada8f1ec
viperchill <viperchill@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-19 21:19:55 +0000

    git-svn-id: https://plugins.svn.wordpress.org/viperbar/trunk@374978 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ header.php
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+	jQuery(document).ready(function() {
+		<?php require("bar_structure.php"); ?>
+
+		
+		jQuery('body').prepend(html);
+		
+		<?php require("bar_actions.php"); ?>
+	});
+</script>
+<?php require("css.php"); ?>
