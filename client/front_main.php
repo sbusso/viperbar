77a4da09433f63cc76725056f53ecf0fa05763b8
viperchill <viperchill@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-22 21:31:26 +0000

    git-svn-id: https://plugins.svn.wordpress.org/viperbar/tags/2.1@427336 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- client/front_main.php
+++ client/front_main.php
@@ -347,7 +347,10 @@ function viperbar_get_mailchimp($options, $button_style) {
 	include_once WP_PLUGIN_DIR . '/viperbar/includes/mailchimp/inc/MCAPI.class.php';
 	$mailchimp_id = trim($options['form_id_mailchimp']);
 	$mailchimp_list = trim($options['form_id_mailchimp_list']);
-	$ViperBar_api = new MCAPI( $mailchimp_id );
+	
+	if (!class_exists(MCAPI)) {
+		$ViperBar_api = new MCAPI( $mailchimp_id );
+	}
 	$merge_vars = $ViperBar_api->listMergeVars( $mailchimp_list );
 
 	if (!$ViperBar_api->errorCode) {
